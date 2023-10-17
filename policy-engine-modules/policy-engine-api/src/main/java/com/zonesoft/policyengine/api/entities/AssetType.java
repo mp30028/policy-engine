7f7859009e956ee9def14371d5b0b6895f281168
mp30028 <mp30028@gmail.com>
2023-10-17 13:07:21 +0100

    extended integration test to retrieve AssetType from testcontainer db

--- policy-engine-modules/policy-engine-api/src/main/java/com/zonesoft/policyengine/api/entities/AssetType.java
+++ policy-engine-modules/policy-engine-api/src/main/java/com/zonesoft/policyengine/api/entities/AssetType.java
@@ -1,9 +1,16 @@
 package com.zonesoft.policyengine.api.entities;
 
+import static com.zonesoft.policyengine.api.utilities.ToStringBuilder.lastLine;
+import static com.zonesoft.policyengine.api.utilities.ToStringBuilder.line;
+
+import com.zonesoft.policyengine.api.utilities.ToStringBuilder;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "t_asset_type")
 public class AssetType {
 
 	private Long id;
@@ -33,6 +40,17 @@ public class AssetType {
 	
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	
+	@Override
+	public String toString(){
+		ToStringBuilder b = new ToStringBuilder();
+		return b.build(
+				line("id", id),
+				line("assetType", assetType),
+				lastLine("description", description)
+				
+		);
 	}
 	
 }
--- policy-engine-modules/policy-engine-api/src/test/java/com/zonesoft/policyengine/api/repositories/AssetTypeRepositoryTest.java
+++ policy-engine-modules/policy-engine-api/src/test/java/com/zonesoft/policyengine/api/repositories/AssetTypeRepositoryTest.java
@@ -2,16 +2,53 @@ package com.zonesoft.policyengine.api.repositories;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.zonesoft.policyengine.api.entities.AssetType;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
 class AssetTypeRepositoryTest extends AbstractMySqlContainer{
 	private static final Logger LOGGER = LoggerFactory.getLogger(AssetTypeRepositoryTest.class);
 
+	@Autowired AssetTypeRepository repository;
+	
 	@Test
+	@Order(1)
+	@DisplayName("Check test-container has been instantiated")	
 	void testContainerIsCreated() {
 		assertNotNull(MYSQL_CONTAINER);
 		LOGGER.debug("FROM AssetTypeRepositoryTest.testContainerIsCreated: Test-Container was started on host={} and listening on port={}", MYSQL_CONTAINER.getHost(), MYSQL_CONTAINER.getFirstMappedPort());
 	}
+	
+	@Test
+	@Order(2)
+	@DisplayName("Check repository gets autowired")
+	void testRepoistoryIsWiredUp() {
+		assertNotNull(repository);
+	}
+	
+	@Test
+	@Order(3)
+	@DisplayName("Check connectivity to db via repository and fetch a few records and check the number returned is as expected")	
+	void testFindAll() {
+		int EXPECTED_NO_OF_RECORDS = 5;
+		List<AssetType> findAllResults = repository.findAll();
+		assertEquals(EXPECTED_NO_OF_RECORDS, findAllResults.size());
+		LOGGER.debug("FROM AssetTypeRepositoryTest.testFindAll: repository.findAll returned {} records", findAllResults.size());
+		for (AssetType result: findAllResults) {
+			LOGGER.debug("FROM AssetTypeRepositoryTest.testFindAll: AssetType =  {}",result);
+		}
+	}
+	
 }
