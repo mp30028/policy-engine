09533f647692d2823784a6d75b5ddb8d2b8d66aa
mp30028 <mp30028@gmail.com>
2023-10-18 06:22:36 +0100

    clean up default files created by npm

--- policy-engine-modules/policy-engine-ui/frontend/src/App.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Routes, Route, NavLink, Outlet } from 'react-router-dom';
+import AppHome from "./Home";
+const App = () => {	return (
+		<>
+		    <h1>Policy-Engine-App</h1>
+		    
+			<nav>					
+				<NavLink to="/" >Home</NavLink><br /><br />
+			</nav>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+			<Routes>
+				<Route path="/" element={<AppHome />} />
+				<Route index element={<AppHome />} />
+				<Route path="*" element={<p>There's nothing here: 404!</p>} />
+			</Routes>
+			
+			<main>
+				<Outlet />
+			</main>
+		</>
+	);
+};
 
 export default App;
