3b6a81c6c264814487f7552fb9375a0b1246a858
mp30028 <mp30028@gmail.com>
2023-10-24 21:32:44 +0100

    tabbed home created and added to router

--- policy-engine-modules/policy-engine-ui/frontend/src/App.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/App.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
-import PolicyEngineApp from "./components/policy-engine-app/PolicyEngineApp"
+import Home from './Home';
 const App = () => {	return (
 		<>
 			<Routes>
-				<Route path="/" element={<PolicyEngineApp />} />			
+				<Route path="/" element={<Home />} /> 
 				<Route path="*" element={<p>There's nothing here: 404!</p>} />			
 			</Routes>
 			
--- policy-engine-modules/policy-engine-ui/frontend/src/Home.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/Home.js
@@ -1,13 +1,29 @@
 import * as React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "./static/css/ZonesoftTabs.css"
+import PolicyEngineApp from "./components/policy-engine-app/PolicyEngineApp"
 
 function Home() {
-  return (
-    <>
-    	<pre>
-    	--Policy Engine Home Placeholder--
-    	</pre>
-    </>
-  );
+	return (
+		<Tabs>
+			<TabList>
+				<Tab>Setup Policies</Tab>
+				<Tab>Manage Assets</Tab>
+				<Tab>Examples etc</Tab>
+			</TabList>
+
+			<TabPanel>
+				<PolicyEngineApp />
+			</TabPanel>
+			<TabPanel>
+				<h2>Managing Assets etc will go here</h2>
+			</TabPanel>
+			<TabPanel>
+				<h2>Examples etc will go here</h2>
+			</TabPanel>
+
+		</Tabs>
+	);
 };
 
 export default Home;
