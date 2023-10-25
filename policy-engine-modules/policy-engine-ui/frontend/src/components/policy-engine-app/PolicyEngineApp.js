4a61771dc7b231dfe1309a60c0c1936d75129dad
mp30028 <mp30028@gmail.com>
2023-10-25 18:28:11 +0100

    Extended AccordionItem (with formatting and additional attributes)

--- policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/PolicyEngineApp.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/PolicyEngineApp.js
@@ -1,10 +1,9 @@
 import React, {useEffect, useState } from 'react';
-//import { Accordion, AccordionItem as Item} from '@szhsin/react-accordion';
-import { Accordion, AccordionItem} from '@szhsin/react-accordion';
-import chevronDown from "../../static/icons/chevron-down.svg";
-import styles from "../../static/css/accordion.module.css";
+import { Accordion} from '@szhsin/react-accordion';
 import ApiClientConfigs from "../../configurations/ApiClientConfigsClass";
 import DataService from "../../data-services/DataServiceClass";
+import AccordionItem from './accordion/AccordionItem';
+import styles from "../../static/css/accordion.module.css";
 //import Policies from "./policies/Policies";
 
 function PolicyEngineApp() {
@@ -19,30 +18,11 @@ function PolicyEngineApp() {
 	return (
 		<div className={styles.accordion}>
 			<Accordion allowMultiple >
-				{assetTypes.map(at =>
-					<div >
-						<AccordionItem
-							key={at.id}
-							header={
-								<div >
-									<p className={styles.title}>
-										{at.name}
-										<img className={styles.chevron} src={chevronDown} alt="Chevron Down" />
-									</p>
-									<p className={styles.description}>
-										{at.description}
-									</p>
-								</div>
-							}
-							buttonProps={{ className: ({ isEnter }) => `${styles.itemBtn} ${isEnter && styles.itemBtnExpanded}` }}
-							contentProps={{ className: styles.itemContent }}
-							panelProps={{ className: styles.itemPanel }}
-						>
-							---- Policies for the Asset-Type above to go here -----
-{/*							<Policies /> */}
-						</AccordionItem>
-						<hr style={{ width: "1000px", marginLeft: "0" }} />
-					</div>
+				{assetTypes.map(at => 							
+					<AccordionItem header={at.name} label="Asset-Type" description={at.description} key={at.id}>						
+						---- Policies for the Asset-Type above to go here -----
+						{/*<Policies />*/}							
+					</AccordionItem>
 				)}
 			</Accordion>
 		</div>
