1f2d5c98200b2f00495a336c0f91d211192e4746
mp30028 <mp30028@gmail.com>
2023-10-25 14:21:37 +0100

    Created first level of accordion based layout

--- policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/PolicyEngineApp.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/PolicyEngineApp.js
@@ -1,34 +1,51 @@
-import React, {useState} from 'react';
-import "../../static/css/Zonesoft.css"
-import AssetTypes from "./asset-types/AssetTypes";
-import Policies from "./policies/Policies";
-import Attributes from "./attributes/Attributes";
-
+import React, {useEffect, useState } from 'react';
+//import { Accordion, AccordionItem as Item} from '@szhsin/react-accordion';
+import { Accordion, AccordionItem} from '@szhsin/react-accordion';
+import chevronDown from "../../static/icons/chevron-down.svg";
+import styles from "../../static/css/accordion.module.css";
+import ApiClientConfigs from "../../configurations/ApiClientConfigsClass";
+import DataService from "../../data-services/DataServiceClass";
+//import Policies from "./policies/Policies";
 
 function PolicyEngineApp() {
-	
-	const [selectedAssetTypes, setSelectedAssetTypes] = useState([]);
-	const [selectedPolicy, setSelectedPolicy] = useState([]);
-	
+	const ENTITY_NAME = "assetType";
+	const [assetTypes, setAssetTypes] = useState([]);
+			
+	useEffect(() => {
+		const dataService = new DataService(new ApiClientConfigs(),ENTITY_NAME);
+		dataService.fetchAll().then((data) => setAssetTypes(data));
+	}, [setAssetTypes]);
+			
 	return (
-		<table className="zsft-table" style={{ width: "100%" }}>
-			<thead>
-				<tr>
-					<th colSpan="2">
-						Policy Management App
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td colSpan={2}><AssetTypes selectedAssetTypesSetter={setSelectedAssetTypes} /></td>
-				</tr>
-				<tr>
-					<td><Policies selectedAssetTypes={selectedAssetTypes} selectedPolicySetter={setSelectedPolicy}/></td>
-					<td><Attributes selectedPolicy={selectedPolicy} /></td>
-				</tr>
-			</tbody>
-		</table>
+		<div className={styles.accordion}>
+			<Accordion allowMultiple >
+				{assetTypes.map(at =>
+					<div >
+						<AccordionItem
+							key={at.id}
+							header={
+								<div >
+									<p className={styles.title}>
+										{at.name}
+										<img className={styles.chevron} src={chevronDown} alt="Chevron Down" />
+									</p>
+									<p className={styles.description}>
+										{at.description}
+									</p>
+								</div>
+							}
+							buttonProps={{ className: ({ isEnter }) => `${styles.itemBtn} ${isEnter && styles.itemBtnExpanded}` }}
+							contentProps={{ className: styles.itemContent }}
+							panelProps={{ className: styles.itemPanel }}
+						>
+							---- Policies for the Asset-Type above to go here -----
+{/*							<Policies /> */}
+						</AccordionItem>
+						<hr style={{ width: "1000px", marginLeft: "0" }} />
+					</div>
+				)}
+			</Accordion>
+		</div>
 	);
 }
 
