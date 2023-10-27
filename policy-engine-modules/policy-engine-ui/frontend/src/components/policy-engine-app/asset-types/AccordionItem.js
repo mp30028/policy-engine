6ceff21cf254cb4b486578247b9dd90415d2b08d
mp30028 <mp30028@gmail.com>
2023-10-27 06:34:12 +0100

    inline editing within ui enabled. Saving on server to be completed

--- policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/asset-types/AccordionItem.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/asset-types/AccordionItem.js
@@ -1,28 +1,71 @@
-import React from 'react';
-import { AccordionItem as Item} from '@szhsin/react-accordion';
+import React, { useState } from 'react';
+import { AccordionItem as Item } from '@szhsin/react-accordion';
 import chevronDown from "../../../static/icons/chevron-down.svg";
+import "../../../static/css/InlineEdit.css"
 import styles from "./accordion.module.css";
+import EasyEdit from "react-easy-edit";
+export default function AccordionItem({ assetType, label, ...rest }) {
+	//const httpTaskCallback = () => {};
 
-export default function AccordionItem({ header, label, description, ...rest }){
+	console.log("FROM AccordionItem: assetType=", assetType);
+	//	const [editValue, setEditValue] = useState('');
+	//	
+	//	setEditValue((header) ? header : '');
+	//	
+	const save = (newValue) => {
+		console.log("FROM AccordionItem.save", "key=", newValue.key, "value", newValue.value);
+	}
+	const cancel = () => {
+		console.log("FROM AccordionItem.cancel");
+	}
+
+	const NameEdit = () => {
+		return (
+			<EasyEdit
+				type="text"
+				value={assetType.name}
+				onSave={(newValue) => save({ key: 'name', value: newValue })}
+				onCancel={cancel}
+				saveButtonLabel=""
+				cancelButtonLabel=""
+				attributes={{ name: "name-edit", id: assetType.id }}
+			/>
+		)
+	}
+
+	const DescriptionEdit = () => {
+		return (
+			<p className={styles.description}>
+				<EasyEdit
+					type="text"
+					value={assetType.description}
+					onSave={(newValue) => save({ key: 'description', value: newValue })}
+					onCancel={cancel}
+					saveButtonLabel=""
+					cancelButtonLabel=""
+					attributes={{ name: "description-edit", id: assetType.id }}
+				/>
+			</p>
+		)		
+	}
+
 	return (
 		<>
-		  <Item
-		    {...rest}
-		    header={
-		      <>
-		      	<span className={styles.label}>{label}: </span>
-		        {header}
-				<p className={styles.description}>
-					{description}
-				</p>
-				<img className={styles.chevron} src={chevronDown} alt="Chevron Down" />
-		      </>
-		    }
-		    className={styles.item}
-		    buttonProps={{className: ({ isEnter }) =>`${styles.itemBtn} ${isEnter && styles.itemBtnExpanded}`}}
-		    contentProps={{ className: styles.itemContent }}
-		    panelProps={{ className: styles.itemPanel }}
-		  />		  
-  		</>
-  );
+			<Item
+				{...rest}
+				header={
+					<>
+						<span className={styles.label}>{label + '-' + assetType.id}: </span>
+						<NameEdit />						
+						<DescriptionEdit />						
+						<img className={styles.chevron} src={chevronDown} alt="Chevron Down" />
+					</>
+				}
+				className={styles.item}
+				buttonProps={{ className: ({ isEnter }) => `${styles.itemBtn} ${isEnter && styles.itemBtnExpanded}` }}
+				contentProps={{ className: styles.itemContent }}
+				panelProps={{ className: styles.itemPanel }}
+			/>
+		</>
+	);
 }
--- policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/asset-types/AssetTypes.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/asset-types/AssetTypes.js
@@ -5,6 +5,9 @@ import DataService from "../../../classes/data-services/DataService.class";
 import AccordionItem from './AccordionItem';
 import styles from "./accordion.module.css";
 import Policies from "../policies/Policies";
+//import {RIEInput} from 'riek'
+//import _ from 'lodash'
+
 
 function AssetTypes() {
 	const ENTITY_NAME = "assetType";
@@ -14,12 +17,14 @@ function AssetTypes() {
 		const dataService = new DataService(new ApiClientConfigs(),ENTITY_NAME);
 		dataService.fetchAll().then((data) => setAssetTypes(data));
 	}, [setAssetTypes]);
+	
+//					<AccordionItem header={at.name} label="Asset-Type" description={at.description} assetTypeId={at.id} key={at.id}>
 			
 	return (
 		<div className={styles.accordion}>
 			<Accordion allowMultiple >
 				{assetTypes.map(at => 							
-					<AccordionItem header={at.name} label="Asset-Type" description={at.description} key={at.id}>						
+					<AccordionItem assetType={at}  label="Asset-Type" key={at.id}>						
 						<Policies assetType={at}/>
 						<hr style={{ width: "1000px", marginLeft: "0" }} />
 					</AccordionItem>
