5f8f77dd79288d7ac16d2e972daae266655a26de
mp30028 <mp30028@gmail.com>
2023-10-29 01:17:25 +0000

    For updates UI elements added and working (but still need to build API)

--- policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/asset-types/AccordionItem.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/asset-types/AccordionItem.js
@@ -1,65 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AccordionItem as Item } from '@szhsin/react-accordion';
 import chevronDown from "../../../static/icons/chevron-down.svg";
 import "../../../static/css/InlineEdit.css"
 import styles from "./accordion.module.css";
 import EasyEdit from "react-easy-edit";
-export default function AccordionItem({ assetType, label, ...rest }) {
-	//const httpTaskCallback = () => {};
 
-	console.log("FROM AccordionItem: assetType=", assetType);
-	//	const [editValue, setEditValue] = useState('');
-	//	
-	//	setEditValue((header) ? header : '');
-	//	
-	const save = (newValue) => {
-		console.log("FROM AccordionItem.save", "key=", newValue.key, "value", newValue.value);
-	}
-	const cancel = () => {
-		console.log("FROM AccordionItem.cancel");
-	}
+//const AccordionItem = ({ assetType, label, ...rest }) => {
+const AccordionItem = (props) => {	
+	console.log("FROM AccordionItem: props.assetType=", props.assetType);
+	
+	const [assetType, setAssetType] = useState(null);
+	
+	useEffect(() => {
+		setAssetType(props.assetType);
+	}, [props.assetType, setAssetType])
 
-	const NameEdit = () => {
-		return (
-			<EasyEdit
-				type="text"
-				value={assetType.name}
-				onSave={(newValue) => save({ key: 'name', value: newValue })}
-				onCancel={cancel}
-				saveButtonLabel=""
-				cancelButtonLabel=""
-				attributes={{ name: "name-edit", id: assetType.id }}
-			/>
-		)
-	}
+	useEffect(() => {
+		console.log("FROM AccordionItem:useEffect[assetType]: assetType=", (assetType)?assetType.toString():'null');
+	}, [assetType])
 
-	const DescriptionEdit = () => {
-		return (
-			<p className={styles.description}>
-				<EasyEdit
-					type="text"
-					value={assetType.description}
-					onSave={(newValue) => save({ key: 'description', value: newValue })}
-					onCancel={cancel}
-					saveButtonLabel=""
-					cancelButtonLabel=""
-					attributes={{ name: "description-edit", id: assetType.id }}
-				/>
-			</p>
-		)		
-	}
-
+	
+	const dataUpdateHandler = (newValue) => {
+		console.log("FROM AccordionItem.dataUpdateHandler", "key=", newValue.key, ", value=", newValue.value);
+		var updated = assetType;
+		updated[newValue.key]= newValue.value;
+		console.log("FROM AccordionItem.dataUpdateHandler", "updated=", updated);
+		setAssetType(updated);
+		if (props.onDataChange)  props.onDataChange({type: 'UPDATE', data: updated});
+	};
+	
 	return (
 		<>
 			<Item
-				{...rest}
+				{...props.rest}
 				header={
-					<>
-						<span className={styles.label}>{label + '-' + assetType.id}: </span>
-						<NameEdit />						
-						<DescriptionEdit />						
-						<img className={styles.chevron} src={chevronDown} alt="Chevron Down" />
-					</>
+					(assetType) ?
+						<>
+							<span className={styles.label}>{props.label + '-' + assetType.id}: </span>
+							<NameEdit assetType={assetType} onChange={dataUpdateHandler} />						
+							<DescriptionEdit assetType={assetType} onChange={dataUpdateHandler}/>						
+							<img className={styles.chevron} src={chevronDown} alt="Chevron Down" />
+						</>
+						:
+						<>{/* ELSE empty */}</>
 				}
 				className={styles.item}
 				buttonProps={{ className: ({ isEnter }) => `${styles.itemBtn} ${isEnter && styles.itemBtnExpanded}` }}
@@ -69,3 +52,58 @@ export default function AccordionItem({ assetType, label, ...rest }) {
 		</>
 	);
 }
+
+
+const NameEdit = (props) => {	
+	const save = (newValue) => {
+//		console.log("FROM AccordionItem.save", "key=", newValue.key, "value", newValue.value);
+		props.onChange(newValue);
+	};
+		
+	const cancel = () => {
+		console.log("FROM AccordionItem.cancel");
+	};		
+		
+	return (
+		<span>
+			<EasyEdit
+				type="text"
+				value={props.assetType.name}
+				onSave={(newValue) => save({ key: 'name', value: newValue })}
+				onCancel={cancel}
+					saveButtonLabel=""
+					cancelButtonLabel=""			
+				attributes={{ name: "name-edit", id: props.assetType.id }}
+			/>
+		</span>
+	);
+}
+
+
+const DescriptionEdit = (props) => {
+	const save = (newValue) => {
+//		console.log("FROM AccordionItem.save", "key=", newValue.key, "value", newValue.value);
+		props.onChange(newValue);
+	}
+	
+	const cancel = () => {
+		console.log("FROM AccordionItem.cancel");
+	}		
+	return (
+		<span className={styles.description}>
+			<EasyEdit
+				type="text"
+				value={props.assetType.description}
+				onSave={(newValue) => save({ key: 'description', value: newValue })}
+				onCancel={cancel}
+				saveButtonLabel=""
+				cancelButtonLabel=""
+				attributes={{ name: "description-edit", id: props.assetType.id }}
+			/>
+		</span>
+	)		
+}
+
+
+	
+export default AccordionItem;
--- policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/asset-types/AssetTypes.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/components/policy-engine-app/asset-types/AssetTypes.js
@@ -5,34 +5,69 @@ import DataService from "../../../classes/data-services/DataService.class";
 import AccordionItem from './AccordionItem';
 import styles from "./accordion.module.css";
 import Policies from "../policies/Policies";
-//import {RIEInput} from 'riek'
-//import _ from 'lodash'
-
 
 function AssetTypes() {
 	const ENTITY_NAME = "assetType";
 	const [assetTypes, setAssetTypes] = useState([]);
+	const [isSavePending, setIsSavePending] = useState(false);
 			
 	useEffect(() => {
 		const dataService = new DataService(new ApiClientConfigs(),ENTITY_NAME);
 		dataService.fetchAll().then((data) => setAssetTypes(data));
 	}, [setAssetTypes]);
 	
-//					<AccordionItem header={at.name} label="Asset-Type" description={at.description} assetTypeId={at.id} key={at.id}>
+	const onDataChangeHandler = (change) =>{
+		console.log("FROM AssetTypes.onDataChangeHandler, update.type", change.type, 'update.data=', change.data );
+		var updated = null;
+		switch (change.type){
+			case 'UPDATE':
+				console.log("FROM AssetTypes.onDataChangeHandler, case 'UPDATE'");
+				updated = assetTypes.map((at) => (at.id === change.data.id) ? change.data : at);
+				break;
+			case 'DELETE':
+				console.log("FROM AssetTypes.onDataChangeHandler, case 'DELETE'");
+				updated = assetTypes.map((at) => (at.id === change.data.id) ? null : at);				
+				break;
+			case 'ADD-NEW':
+				console.log("FROM AssetTypes.onDataChangeHandler, case 'ADD-NEW'");
+				updated = [...assetTypes, change.data];
+				break;
+			default:
+				console.log("FROM AssetTypes.onDataChangeHandler, Something very unexpected has  happened.");
+		}
+		if (updated) setAssetTypes(updated);
+		setIsSavePending((updated)? true : false);
+	}
+	
+	const onSaveAllHandler = () => {
+		setIsSavePending(false);
+	}
 			
 	return (
 		<div className={styles.accordion}>
 			<Accordion allowMultiple >
 				{assetTypes.map(at => 							
-					<AccordionItem assetType={at}  label="Asset-Type" key={at.id}>						
-						<Policies assetType={at}/>
+					<AccordionItem assetType={at} onDataChange={onDataChangeHandler}   label="Asset-Type" key={at.id}>						
+						<Policies assetType={at} />
 						<hr style={{ width: "1000px", marginLeft: "0" }} />
-					</AccordionItem>
-					
+					</AccordionItem>					
 				)}
 			</Accordion>
+			{isSavePending && <SaveAllButton onSave={onSaveAllHandler} data={assetTypes} />}
 		</div>
 	);
+}
+
+const SaveAllButton = (props) =>{
+	
+	const saveAllOnClick = (event) =>{
+		console.log("FROM AssetTypes.saveAllOnClick, event=", event);
+		props.onSave();
+	}
+		
+	return (
+		<input type='button' name='saveAll' id='saveAll' onClick={saveAllOnClick} value='Commit All Changes'/>
+	); 	
 }
 
 export default AssetTypes;
