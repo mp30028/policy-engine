18754c481f3269be907a8d9d78170e80c8f67b77
mp30028 <mp30028@gmail.com>
2023-10-31 11:58:03 +0000

    fix up UI after deletion of old code

--- policy-engine-modules/policy-engine-ui/frontend/src/Home.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/Home.js
@@ -2,27 +2,22 @@ import * as React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import "./static/css/ZonesoftTabs.css"
 import AssetTypes from "./components/policy-engine-app/asset-types/AssetTypes"
-import PolicyEngineApp2 from "./components/policy-engine-app-2/PolicyEngineApp"
 
 function Home() {
 	return (
 		<Tabs>
 			<TabList>
-				<Tab>Setup Policies</Tab>
-				<Tab>Setup Policies (Old Version)</Tab>
-				<Tab>Manage Assets</Tab>
+				<Tab>Manage Asset Types</Tab>
+				<Tab>Manage Policies</Tab>
 				<Tab>Examples etc</Tab>
 			</TabList>
 
 			<TabPanel>
 				<AssetTypes />
 			</TabPanel>
-			<TabPanel>
-				<PolicyEngineApp2 />
-			</TabPanel>
 			
 			<TabPanel>
-				<h2>Managing Assets etc will go here</h2>
+				<h2>Managing Policies will go here</h2>
 			</TabPanel>
 			<TabPanel>
 				<h2>Examples etc will go here</h2>
