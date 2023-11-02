622aafdcd7c2cba518d942ee349da29506de9cd1
mp30028 <mp30028@gmail.com>
2023-11-02 07:06:26 +0000

    outline for managing policies pop-up

--- policy-engine-modules/policy-engine-ui/frontend/src/components/manage-asset-types/policies/Policies.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/components/manage-asset-types/policies/Policies.js
@@ -39,28 +39,64 @@ export default function Policies(props) {
 						</div>
 					</AccordionItem>
 				)}
-				<PoliciesPickList />
+				<PoliciesPickList parentId={(assetType) ? assetType.id : 0} currentlySelectedPolices={policies} ENTITY_NAME={ENTITY_NAME} />
+				<br/>
 			</Accordion>
 		</div>
 	)
 }
 
-const AssignPolicy = React.forwardRef((props, ref) => {		
-	return (
-		<>
-			<AccordionItem header="" onClick={props.onClick} label={"-- click to assign another policy --"} description="" key={-1}></AccordionItem>
-		</>
-	);
-})
-
 const PoliciesPickList = React.forwardRef( (props, ref) => {
+	
+	const [allPolicies, setAllPolicies] = useState([]);
+	const [selectedPolices, setSelectedPolicies] = useState([]);
+	
+	useEffect(() => {
+		const dataService = new DataService(new ApiClientConfigs(),props.ENTITY_NAME);
+		dataService.fetchAll().then((data) => setAllPolicies(data));		
+	}, [setAllPolicies, props.ENTITY_NAME]);	
+	
+	useEffect(() =>{
+		const updateSelectedPolicies = (all, currentlySelected) =>{
+			const updatedSelected = (all.filter(p => props.currentlySelectedPolices.find((q) => q.id === p.id)));
+			setSelectedPolicies(updatedSelected);
+		};		
+		updateSelectedPolicies(allPolicies, props.currentlySelectedPolices);
+	},[props, allPolicies])
+	
+	const isSelected = (id) => {
+		console.log("FROM isSelected id=", id);
+		const findResult = selectedPolices.find((p) => (p.id === id));
+		var returnValue = ((findResult) ? true : false);
+		console.log("FROM isSelected returnValue=", returnValue);
+		return returnValue;
+	}
+
+	const AssignPolicy = React.forwardRef((props, ref) => {		
+		return (
+			<div >
+			      <span className={styles.addNew} onClick={props.onClick} />
+			      <span className={styles.label}>Manage Assigned Policies</span>
+			</div>	
+					
+			
+		);
+	})
+
+	
 	return (
 			<Popup 
 				trigger={open => <AssignPolicy onClick={open} />}
 				modal
 				nested
 			>
-				<span> Popup content </span>
+				<span>
+					 {allPolicies.map(p =>
+					 	<span key={props.parenteId + '-' + p.id}> 
+					 		{p.name} - 	{(isSelected(p.id)).toString()} <br/>
+					 	</span>
+					 )}
+				</span>
 			</Popup>
 	);	
 })
