0fe257ec7262af52adee51b46c9493a6e32c871b
mp30028 <mp30028@gmail.com>
2023-11-11 15:36:10 +0000

    simplified ui architecture completed

--- policy-engine-modules/policy-engine-ui/frontend/src/classes/configurations/LoggerConfigs.class.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/classes/configurations/LoggerConfigs.class.js
@@ -3,12 +3,25 @@ export default class LoggerConfigs{
 	static FALLBACK_LOG_LEVEL = "INFO";		
 
 	static LEVELS = {
-		ManageAssetTypes: "INFO",
+		ManageAssetTypes: { value: "DEBUG", 
+							TextEdit: {value: "DEBUG"}
+		}
 	}
 	
-	getLogLevel = (name) => {
-		let lookupValue = LoggerConfigs.LEVELS[name];
-		return (lookupValue) ? lookupValue : LoggerConfigs.FALLBACK_LOG_LEVEL;
+	getLogLevel = (loggerName) => {
+		const names = loggerName.split(".");
+		var currentNode = LoggerConfigs.LEVELS;
+		for (let n in names){
+			const name = names[n];			
+			const nextNode = currentNode[name];
+			if(nextNode){
+				currentNode = nextNode;				 
+			}else{
+				currentNode = null;
+				break;
+			}
+		}
+		return ((currentNode) ? currentNode.value : LoggerConfigs.FALLBACK_LOG_LEVEL);		
 	}
 	
 }
--- policy-engine-modules/policy-engine-ui/frontend/src/components/manage-asset-types/ManageAssetTypes.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/components/manage-asset-types/ManageAssetTypes.js
@@ -6,8 +6,9 @@ import Logger from '../../classes/logger/Logger.class';
 import Policies from './policies/Policies';
 import styles from "./css/ManageAssetTypes.module.css";
 import chevronDown from "../../static/icons/chevron-down.svg";
+import EasyEdit from "react-easy-edit";
 
-function ManageAssetTypes() {
+const ManageAssetTypes = () => {
 	const MODULE = 	"ManageAssetTypes";
 	const LOGGER = new Logger().getLogger(MODULE);			
 	const ENTITY_NAME = "assetType";
@@ -15,15 +16,18 @@ function ManageAssetTypes() {
 			
 	useEffect(() => {
 		const dataService = new DataService(new ApiClientConfigs(),ENTITY_NAME);
-		dataService.fetchAll().then((data) => setAssetTypes(data));
-		LOGGER.debug("Data fetched successfully")
-	}, [setAssetTypes, LOGGER]);
+		dataService.fetchAll().then((data) => setAssetTypes(data));		
+	}, [setAssetTypes]);
+	
+	useEffect(()=>{
+		LOGGER.debug("FROM ", MODULE, ".assetTypesHook: assetTypes=", assetTypes);		
+	},[assetTypes, LOGGER])
 			
 	return (
 			<Accordion allowMultiple className={styles.accordion} style={{width: '800px'}}>
 				{assetTypes.map(at =>
 					<AccordionItem
-						header={<AccordionItemHeader assetType={at}/>}				
+						header={<AccordionItemHeader assetType={at} />}				
 						headingProps={{ className: styles.header }}
 						panelProps={{className: styles.panel}}
 						buttonProps={{ className: ({ isEnter }) => `${styles.button} ${isEnter && styles.buttonExpanded}` }}
@@ -31,35 +35,56 @@ function ManageAssetTypes() {
 						className={styles.item}
 						key={at.id}
 					>
-						<AccordionItemContent assetType={at} />
+						<AccordionItemContent assetType={at} />						
 					</AccordionItem>									
 				)}				
 			</Accordion>
 	);
-}
+};
+export default ManageAssetTypes;
 
 
-const AccordionItemHeader = (props) => {			
+
+/* --------------------------------------------------------------------------------------------------- */
+const AccordionItemHeader = (props) => {
+	const MODULE = 	"ManageAssetTypes.AccordionItemHeader";
+	const LOGGER = new Logger().getLogger(MODULE);			
+
+	const emptyAssetType = {id:0, name: "", description:"", associatedPolicies:[]}	
+	const [assetType, setAssetType] = useState(emptyAssetType);
+	
+	useEffect(()=>{
+		setAssetType(props.assetType);
+	},[props.assetType])
+		
+	const onChangeHandler = (updatedAssetType) =>{		
+		LOGGER.debug("FROM ", MODULE, ".onChangeHandler: updatedAssetType=", updatedAssetType);
+	}
+	
 	return (
-		<>
-			{(props.assetType) ?							
-				<span className={styles.headerContainer}>
-					<span className={styles.label}>Asset-Type- 
-						<span className={styles.id}>{props.assetType.id}</span>
-					</span>								 
-					<span className={styles.content}>{props.assetType.name}</span>
-					<span className={styles.info}>{props.assetType.description}</span>
-					<img src={chevronDown} alt="Chevron Down" className={styles.chevron}/>									
-				</span>								
-			:
-				<span>
-					{/* ELSE empty */}
+		<div>
+			<div className={styles.headerContainer}>
+				<span className={styles.label}>Asset-Type- 
+					<span className={styles.id}>{assetType.id}</span>
+				</span>								 
+				<span className={styles.content}>						
+					<TextEdit assetTypeProperty="name" assetType={assetType} onChange={onChangeHandler} />
 				</span>
-			}																										
-		</>
+				<span className={styles.info}>
+					<TextEdit assetTypeProperty="description" assetType={assetType} onChange={onChangeHandler} />
+				</span>
+				<img src={chevronDown} alt="Chevron Down" className={styles.chevron}/>									
+			</div>
+			<div  onClick={event => event.stopPropagation()}>
+				<SaveCancelButtons assetType={assetType} />
+			</div>
+		</div>
 	);
 }
 
+
+
+/* --------------------------------------------------------------------------------------------------- */
 const AccordionItemContent = (props) =>{
 	return (
 		<>
@@ -68,4 +93,68 @@ const AccordionItemContent = (props) =>{
 	);	
 }
 
-export default ManageAssetTypes;
+
+
+/* --------------------------------------------------------------------------------------------------- */
+const TextEdit = (props) => {
+	const MODULE = 	"ManageAssetTypes.TextEdit";
+	const LOGGER = new Logger().getLogger(MODULE);			
+	const emptyAssetType = {id:0, name: "", description:"", associatedPolicies:[]}		
+	const [assetType, setAssetType] = useState(emptyAssetType);
+	
+	useEffect(()=>{
+		setAssetType(props.assetType);
+	},[props.assetType])
+	
+	const save = (newValue) => {
+		LOGGER.debug("FROM ", MODULE, ".save: newValue=", newValue);
+		let updatedAssetType = assetType;
+		updatedAssetType[props.assetTypeProperty] = newValue;
+		props.onChange(updatedAssetType);
+		setAssetType(updatedAssetType);
+		LOGGER.debug("FROM ", MODULE, ".save: updatedAssetType=", updatedAssetType);
+	};
+
+	const cancel = () => {
+		LOGGER.debug("FROM ", MODULE, ".cancel");		
+	};		
+		
+	return (
+		<span onClick={event => event.stopPropagation()}>
+			<EasyEdit
+				type="text"
+				value={assetType[props.assetTypeProperty]}
+				onSave={(newValue) => save(newValue)}
+				onCancel={cancel}
+				saveOnBlur={true}
+				hideSaveButton={true}
+				hideCancelButton={true}
+				saveButtonLabel=""
+				cancelButtonLabel=""					
+			/>
+		</span>
+	);
+}
+
+
+
+/* --------------------------------------------------------------------------------------------------- */
+const SaveCancelButtons = (props) => {
+	const MODULE = 	"ManageAssetTypes.SaveCancelButtons";
+	const LOGGER = new Logger().getLogger(MODULE);			
+
+	const emptyAssetType = {id:0, name: "", description:"", associatedPolicies:[]}	
+	const [assetType, setAssetType] = useState(emptyAssetType);
+	
+	useEffect(()=>{
+		setAssetType(props.assetType);
+	},[props.assetType])
+
+	return (
+		<span >
+			<input type='button' id='saveButton' name= 'saveButton' value='Save' />
+			<input type='button' id='cancelButton' name= 'saveButton' value='Cancel' />
+		</span>
+	);
+		
+}
