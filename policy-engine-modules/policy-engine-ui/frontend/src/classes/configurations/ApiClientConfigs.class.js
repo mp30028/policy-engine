ca10d20a1b4f6d16051fe23fab5ee7bb753270d2
mp30028 <mp30028@gmail.com>
2023-11-29 18:04:20 +0000

    ui saddled to run within docker

--- policy-engine-modules/policy-engine-ui/frontend/src/classes/configurations/ApiClientConfigs.class.js
+++ policy-engine-modules/policy-engine-ui/frontend/src/classes/configurations/ApiClientConfigs.class.js
@@ -1,13 +1,49 @@
 
 export default class ApiClientConfigs{
-	static baseUrl = "http://localhost:8083/policy-engine/api"
+	
 	static paths = {
 		assetType: "/asset-type",
 		attribute: "/attribute",
 		policy: "/policy"
 	}
 	
-	getUrl = (pathName) =>{
-		return ApiClientConfigs.baseUrl + ApiClientConfigs.paths[pathName];
+	constructor(){
+		const allEnvVarsSet = (
+			("REACT_APP_ENV_API_PROTOCOL" in process.env) &&
+			("REACT_APP_ENV_API_HOST" in process.env) &&
+			("REACT_APP_ENV_API_PORT" in process.env) &&
+			("REACT_APP_ENV_API_CONTEXT_PATH" in process.env)
+		);
+		if (allEnvVarsSet){
+			this.envVars = {
+				REACT_APP_ENV_API_PROTOCOL: `${process.env.REACT_APP_ENV_API_PROTOCOL}`,
+				REACT_APP_ENV_API_HOST: `${process.env.REACT_APP_ENV_API_HOST}`,
+				REACT_APP_ENV_API_PORT: `${process.env.REACT_APP_ENV_API_PORT}`,
+				REACT_APP_ENV_API_CONTEXT_PATH: `${process.env.REACT_APP_ENV_API_CONTEXT_PATH}`,
+			}
+		}else{
+			this.envVars = this.#synchronousFetchEnvVars();
+		}
+	}
+		
+	#synchronousFetchEnvVars() {
+		const fetchUrl = "/policy-engine/env-vars";
+   		const xhr = new XMLHttpRequest();
+   		xhr.open('GET', fetchUrl, false);
+   		xhr.send(null);
+   		if (xhr.status === 200) {
+      		return JSON.parse(xhr.responseText);
+   		} else {
+      		throw new Error('Request failed: ' + xhr.statusText);
+   		}
+	}		
+		
+	
+	#baseUrl = () =>{
+		return this.envVars.REACT_APP_ENV_API_PROTOCOL + "://" + this.envVars.REACT_APP_ENV_API_HOST + ":" + this.envVars.REACT_APP_ENV_API_PORT + this.envVars.REACT_APP_ENV_API_CONTEXT_PATH;	
+	};
+
+	getUrl = (pathName) => {
+		return this.#baseUrl() + ApiClientConfigs.paths[pathName];
 	}
 }
---
+++ policy-engine-modules/policy-engine-ui/frontend/src/examples/example-5/Example5.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+const Example5 = () => {
+	const MODULE = 	"Example-5";
+	const [envVars, setEnvVars] = useState("");	
+	
+	useEffect(() => {		
+		const fetchEnvVars = async() => {
+//			const fetchUrl = "http://localhost:1111/policy-engine/env-vars";			                  
+			const fetchUrl = "/policy-engine/env-vars";
+			const response = await fetch(
+				fetchUrl, 
+				{	method: 'GET',
+					headers: {
+						'Content-Type': 'application/json;charset=UTF-8',
+						'Accept': 'application/json, text/plain'
+					}
+				}
+			);
+			const returnedJson = await response.json();
+			console.log("returnedJson=", returnedJson)
+			return returnedJson;
+		}
+		
+		fetchEnvVars().then((data) => setEnvVars(data));		
+	}, [setEnvVars]);	
+	
+	return (
+		<div>
+			Hello from {MODULE}<br/><br/><br/>
+
+			<h3>Environment-Variables: </h3>
+			REACT_APP_ENV_API_PROTOCOL: {envVars.REACT_APP_ENV_API_PROTOCOL} <br/>
+			REACT_APP_ENV_API_HOST: {envVars.REACT_APP_ENV_API_HOST}<br/>
+			REACT_APP_ENV_API_PORT: {envVars.REACT_APP_ENV_API_PORT}<br/>
+			REACT_APP_ENV_API_PORT: {envVars.REACT_APP_ENV_API_CONTEXT_PATH}<br/>
+		</div>
+	);
+};
+
+export default Example5;
+
+
